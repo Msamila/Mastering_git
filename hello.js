fbc35cde9129dd862da9c15298906e41cfc21bd6
Msamila <danmsamila1@gmail.com>
2025-07-08 07:37:20 +0200

    edit hello

--- hello.js
+++ hello.js
@@ -1,2 +1,10 @@
 console.log('Hello, World!');
 // This is a simple JavaScript program that prints "Hello, World!" to the console.
+console.log('text, World!');
+// This is a simple JavaScript program that prints "Hello, World!" to the console.
+console.log('Hello, git!');
+// This is a simple JavaScript program that prints "Hello, git!" to the console.
+console.log('t');
+// This is a simple JavaScript program that prints "t" to the console.
+console.log('all student must be aware of this behaviour i have added for the feature branch');
+// This is a simple JavaScript program that prints "all student must be aware of this behaviour
